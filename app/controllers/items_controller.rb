09765202c6e3c14852d487a029b2017ab9f4a2b0
ezra.kipchumba@student.moringaschool.com <ezra.kipchumba@student.moringaschool.com>
2022-11-25 00:06:39 +0300

    complete

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,7 @@
 class ItemsController < ApplicationController
+    
+    def index
+      items = Item.all.order(price: :desc)
+      render json: items, include: :user, except: [:created_at, :updated_at]
+    end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,21 @@
 class UsersController < ApplicationController
+
+rescue_from ActiveRecord::RecordNotFound, with: :record_not_found_response
+    
+
+    def show
+        user = find_user
+        render json: user, except: [:created_at, :updated_at], include: :items
+    end
+
+
+
+    private
+    def find_user
+        User.find_by(id: params[:id])
+    end
+
+    def record_not_found_response
+        render json: { error: "user not found" }, status: :not_found
+    end
 end
